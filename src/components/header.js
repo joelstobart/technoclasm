afe3f2f082ecb96c7279e13c0ff0eaa4c4333fd0
Joel Stobart <joel.stobart@landregistry.gov.uk>
2021-12-28 21:11:46 +0000

    Trying to solve a couple of iPhone problems

--- src/components/header.js
+++ src/components/header.js
@@ -5,39 +5,31 @@ import logoMark from '../images/t.svg'
 import '../styles/header.css'
 
 const Header = ({ siteTitle }) => (
-    <header style={{
-      position: `sticky`,
-      background: `black`,
-      padding: `1.5em 1.0875rem 2em`,
-      top: 0,
-      zIndex: 999,
-    }}>
-      <div style={{
-        margin: `0 auto`,
-        maxWidth: 960}}>
-
-      <img
-        src={logoMark}
-        alt="Technoclasm T logo-mark"
-        height="120rem"
-        width="120rem"
-        align="left"
-        style={{paddingRight:`20px`}}
-      />
-      <h1 style={{ margin: 0, width: `600px`}}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <h2>
-        The Digital Transformation Consultancy
-      </h2></div>
+    <header>
+      <div>
+        <img
+          src={logoMark}
+          alt="Technoclasm T logo-mark"
+          height="120rem"
+          width="120rem"
+          align="left"
+          style={{paddingRight:`20px`}}
+        />
+        <h1 style={{ margin: 0, maxWidth: `600px`}}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+        <h2>
+          The Digital Transformation Consultancy
+        </h2>
+      </div>
     </header>
 
 )
